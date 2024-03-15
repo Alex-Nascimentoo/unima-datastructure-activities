d58bfda9f747db30000479fbd673964ab80b5823
Alex Nacimento <alexdavi150@gmail.com>
2024-03-15 09:40:29 -0300

    feat: create me files

---
+++ me1-alex-nascimento/Main.java
@@ -0,0 +1,30 @@
+import Vector.Vector;
+import Models.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+        Student s4 = new Student();
+
+        s1.setName("Alex");
+        s2.setName("Davi");
+        s3.setName("Gomes");
+        s4.setName("asdfjk");
+
+        Vector studentList = new Vector();
+
+        studentList.add(s1);
+        studentList.add(s2);
+        studentList.add(1, s3);
+
+        String result = studentList.toString();
+
+        System.out.println(result);
+
+//        Tests tests = new Tests();
+//
+//        tests.testAll();
+    }
+}
